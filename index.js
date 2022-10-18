f3abce47fa6ba5973d6d694d140274eaf9807ac7
Amien-byte <amienaziz55@gmail.com>
2022-10-18 21:06:36 +0700

    menambah halaman about. sudah ada title. option pada bagian kanan akan diganti menjadi menu kedua

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const backgroundEl = document.querySelector('.background')
 const windowWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 const windowHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 const backgroundContainer = document.querySelector('.background')
-const dist = 3;
+const menu = document.querySelector('.menu')
+const about = document.querySelector('.about')
+const pageInfo = document.querySelector('.page-info')
+const list = document.querySelector('ul')
+const listItemHeight = 71.3
 
 
-background()
+//setBackground()
 
-function background(){	
+function setBackground(){	
 	
 	for(let usedWidth = 0; usedWidth < windowWidth; usedWidth += 70){
 		for(let usedHeight = 0; usedHeight < windowHeight; usedHeight += 70){
@@ -36,3 +39,41 @@ function background(){
 		}
 	}
 }
+
+function removeBackground(){
+	Array.from(backgroundContainer.children).forEach((child) => child.remove())
+	
+	pageInfo.style.display = 'none'
+}
+
+const aboutButton = document.querySelector('#about')
+aboutButton.addEventListener('click',() => {
+	removeBackground()
+	
+	about.classList.add('show')
+	menu.classList.add('focus')
+})
+
+const worksButton = document.querySelector('#works')
+worksButton.addEventListener('click',() => {
+	removeBackground()
+	
+	menu.classList.add('focus')
+	list.style.transform = 'translateY(-'+ listItemHeight +'px)'
+})
+
+const expertiseButton = document.querySelector('#expertise')
+expertiseButton.addEventListener('click',() => {
+	removeBackground()
+	
+	menu.classList.add('focus')
+	list.style.transform = 'translateY(-'+ listItemHeight*2 +'px)'
+})
+
+const contactButton = document.querySelector('#contact')
+contactButton.addEventListener('click',() => {
+	removeBackground()
+	
+	menu.classList.add('focus')
+	list.style.transform = 'translateY(-'+ listItemHeight*3 +'px)'
+})
